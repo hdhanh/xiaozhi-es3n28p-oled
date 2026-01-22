74d900eb62d0b605435338bfde757f7feda3a1b9
hdhanh <156817855+hdhanh@users.noreply.github.com>
2026-01-22 22:07:01 +0700

    Update config.h

--- main/boards/xiaozhi-ai-iot-vietnam-es3n28p-lcd-2.8/config.h
+++ main/boards/xiaozhi-ai-iot-vietnam-es3n28p-lcd-2.8/config.h
@@ -56,8 +56,8 @@
 #define DISPLAY_SCL_PIN     GPIO_NUM_15
 #define DISPLAY_WIDTH       128
 #define DISPLAY_HEIGHT      64
-#define DISPLAY_MIRROR_X    true
-#define DISPLAY_MIRROR_Y    true
+#define DISPLAY_MIRROR_X    false
+#define DISPLAY_MIRROR_Y    false
 
 // Define SH1106 driver
 #define SH1106
